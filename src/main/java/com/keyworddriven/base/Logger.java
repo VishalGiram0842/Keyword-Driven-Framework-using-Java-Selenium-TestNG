9d32dd39d1f736aab34cede71d1bd2f453e5b5b1
VishalGiram0842 <127740917+VishalGiram0842@users.noreply.github.com>
2025-12-23 19:05:48 +0530

    Create Logger.java

---
+++ src/main/java/com/keyworddriven/base/Logger.java
@@ -0,0 +1,28 @@
+package com.keyworddriven.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerUtil {
+    private static final Logger logger = LogManager.getLogger(LoggerUtil.class);
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logError(String message) {
+        logger.error(message);
+    }
+
+    public static void logWarning(String message) {
+        logger.warn(message);
+    }
+
+    public static void logDebug(String message) {
+        logger.debug(message);
+    }
+
+    public static void logFatal(String message) {
+        logger.fatal(message);
+    }
+}
